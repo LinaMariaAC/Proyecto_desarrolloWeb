2e16eb8636172f2b91edc2760c82c2c551922de4
LinaMariaAC <Linamaria.aristizabalc@gmail.com>
2021-11-12 22:03:05 -0500

    Subiendo solución del proyecto

---
+++ form.js
@@ -0,0 +1,11 @@
+const Contraseña=document.getElementById('contraseña')
+const form=document.getElementById('form')
+
+form.addEventListener('submit', e=>{
+    e.preventDefault()
+    let warning=''
+    let 
+    if(Contraseña.Value.length <6){
+        warning += 'Constraseña incorrecta'
+    }
+})
